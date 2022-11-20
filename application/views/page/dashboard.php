7df6b3842545fd2e3f3a378f9e18964cb85ddc4e
iniryan <riyanadi552@gmail.com>
2022-11-20 15:26:23 +0700

    otewe create dashboard

---
+++ application/views/page/dashboard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- main css -->
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/style.css">
+    
+    <!-- iconscout css -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    
+    <title>Dashboard Test</title>
+</head>
+<body>
+    <nav>
+        <div class="logo-name">
+            
+        </div>
+    </nav>
+</body>
+</html>
