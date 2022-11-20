4e5895ddc199b84564dad9f256358957a8f2609c
rifqyattaufi <rifqy1406@gmail.com>
2022-11-20 21:50:26 +0700

    make login,register view & register method

---
+++ application/controllers/Auth.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+    public function index()
+    {
+        $data['title'] = 'Login Page';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+
+    public function register()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email has already registered!'
+        ]);
+        $this->form_validation->set_rules('password', 'Password', 'required|trim|min_length[3]|matches[repeat]', [
+            'matches' => 'Password dont match!',
+            'min_length' => 'Password too short!'
+        ]);
+        $this->form_validation->set_rules('repeat', 'Repeat Password', 'required|trim|matches[password]', [
+            'matches' => 'Repeat Password dont match!'
+        ]);
+
+        $data['title'] = 'User Registration';
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/register');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'name' => $this->input->post('name'),
+                'email' => htmlspecialchars($this->input->post('email')),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'role' => 1,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please Login</div>');
+            redirect('auth');
+        }
+    }
+}
---
+++ application/views/auth/register.php
@@ -0,0 +1,45 @@
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg my-5 col-lg-7 mx-auto">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Register</h1>
+                            </div>
+                            <form class="user" method="POST" action="<?= base_url('auth/register') ?>">
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full Name" value="<?= set_value('name') ?>">
+                                    <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address" value="<?= set_value('email') ?>">
+                                    <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password" value="<?= set_value('email') ?>">
+                                        <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" class="form-control form-control-user" id="repeat" name="repeat" placeholder="Repeat Password" value="<?= set_value('email') ?>">
+                                        <?= form_error('repeat', '<small class="text-danger pl-3">', '</small>'); ?>
+                                    </div>
+                                </div>
+                                <button href="login.html" type="submit" class="btn btn-primary btn-user btn-block">
+                                    Register Account
+                                </button>
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="<?= base_url('auth/') ?>">Already have an account? Login!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
